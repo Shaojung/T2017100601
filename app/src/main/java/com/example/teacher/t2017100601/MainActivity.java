1f71b6c1183610c7c0e21fab8e2c7aba2db60bf7
Shaojung Lu <shaojung@gmail.com>
2017-10-06 10:22:22 +0800

    use ArrayAdapter

--- app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.teacher.t2017100601;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    final int REQUEST_CODE_ADD = 321;
     String data[] = {"AAA", "BBBB", "CC", "DDDD", "EEEEEEE"};
     ListView lv;
     @Override
@@ -18,5 +22,33 @@ public class MainActivity extends AppCompatActivity {
                     data);
         lv.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add)
+        {
+            Intent it = new Intent(MainActivity.this, AddActivity.class);
+            startActivityForResult(it, REQUEST_CODE_ADD);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD)
+        {
+            if (resultCode == RESULT_OK)
+            {
+
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
