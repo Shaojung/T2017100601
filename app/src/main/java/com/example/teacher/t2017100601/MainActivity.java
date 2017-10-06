16ed80a870437a1346cf2e1ca2ff4835e7f3be2a
Shaojung Lu <shaojung@gmail.com>
2017-10-06 11:21:43 +0800

    modify add Activity

--- app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.teacher.t2017100601;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter(MainActivity.this,
                     android.R.layout.simple_list_item_1,
                     datalist);
+
+        // registerForContextMenu(lv);
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                datalist.remove(i);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
         lv.setAdapter(adapter);
 
     }
@@ -35,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(it, REQUEST_CODE_ADD);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.mycontextmenu, menu);
+        // menu.add("ABC");
+        Log.d("Menu", "onCreateContextMenu" + v.toString());
+
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        return super.onContextItemSelected(item);
     }
 
     @Override
