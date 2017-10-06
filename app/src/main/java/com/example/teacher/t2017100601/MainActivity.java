a5cfe9be6fb0441b30958126f5ba4664c9fadb72
Shaojung Lu <shaojung@gmail.com>
2017-10-06 09:23:49 +0800

    use ArrayAdapter

--- app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
+++ app/src/main/java/com/example/teacher/t2017100601/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.teacher.t2017100601;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String data[] = {"AAA", "BBBB", "CC", "DDDD", "EEEEEEE"};
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    data);
+        lv.setAdapter(adapter);
+
     }
 }
